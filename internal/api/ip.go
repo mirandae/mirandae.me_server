b36131a62b57b3323d7f18f122ce1ce88a23a777
mirandae <mirae@cs.washington.edu>
2017-10-28 02:14:54 -0700

    adding IP location call and logging with location

---
+++ internal/api/ip.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const IP_GEOLOCATION = "http://ip-api.com/json"
+
+type Geolocation struct {
+	Status      string `json:"status"`
+	Country     string `json:"country"`
+	CountryCode string `json:"countryCode"`
+	Region      string `json:"region"`
+	RegionName  string `json:"regionName"`
+	City        string `json:"city"`
+	Zip         string `json:"zip"`
+	Lat         string `json:"lat"`
+	Lon         string `json:"lon"`
+	Timezone    string `json:"timezone"`
+	Isp         string `json:"isp"`
+	Org         string `json:"org"`
+	As          string `json:"as"`
+	Query       string `json:"query"`
+}
+
+func LogLocation(ip string) {
+	resp, err := http.Get(fmt.Sprintf("%s/%s", IP_GEOLOCATION, ip))
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	log.Printf("Connecting from %s", ip)
+	log.Printf("which is : %s", fmt.Sprint(body))
+}
--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"projects/mirandae.me/internal/api"
 	"strings"
 )
 
+const IP_GEOLOCATION = "http://ip-api.com/json"
+
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Println("path: ", r.URL.Path)
-	fmt.Println("scheme: ", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
 	log.Printf("\nNew visitor from %s", r.RemoteAddr)
+	api.LogLocation(fmt.Sprint(r.RemoteAddr))
 	fmt.Fprintf(w, "Hello friend! Looks like you're visiting from %s", r.RemoteAddr)
 }
 
